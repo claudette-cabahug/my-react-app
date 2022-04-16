6817dcd92a79b14a6438a93863211d4748cf218e
Claudette Cabahug <claudette.cabahug@outlook.com>
2022-04-16 23:18:00 +1200

    demonstrate how to use js code in html

--- src/index.js
+++ src/index.js
@@ -1,24 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-// const myfirstelement = <h1>Hello React!</h1>
+const x = 5
 
-const myelement = (
-  <table>
-    <tr>
-      <th>Name</th>
-    </tr>
-    <tr>
-      <td>John</td>
-    </tr>
-    <tr>
-      <td>Elsa</td>
-    </tr>
-  </table>
-)
+const myelement = <h1>{x < 10 ? "Hello" : "Goodbye"}</h1>
 
-// ReactDOM.render(myfirstelement, document.getElementById('root'))
-// ReactDOM.render(<p>Hello</p>, document.getElementById('root'))
 ReactDOM.render(myelement, document.getElementById('root'))
 
 // React renders HTML to the web page by using a function called ReactDOM.render().
