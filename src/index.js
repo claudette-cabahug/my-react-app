eddeabab6f0794d159a918bc86abfea9e48e2e51
Claudette Cabahug <claudette.cabahug@outlook.com>
2022-04-17 00:22:59 +1200

    pass props to react component

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-function Car() {
-  return <h2>Hi, I am Claudette!</h2>
+function Car(props) {
+  return <h1>I am a {props.color} car!</h1>
 }
 
-ReactDOM.render(<Car />, document.getElementById('root'))
+ReactDOM.render(<Car color="red"/>, document.getElementById('root'))
 
 // React renders HTML to the web page by using a function called ReactDOM.render().
 // this is rendered to the div in index.html where root is found
