875b539b7e269fee77ce1f568add16b463dfe580
Claudette Cabahug <claudette.cabahug@outlook.com>
2022-04-17 00:16:53 +1200

    render a component

---
+++ src/Car.jsx
@@ -0,0 +1,8 @@
+// import React from "react"
+// import ReactDOM from "react-dom"
+
+// function Car() {
+//   return <h2>Hi, I am Claudette!</h2>
+// }
+
+// ReactDOM.render(<Car />, document.getElementById('root'))
--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-const x = 5
+function Car() {
+  return <h2>Hi, I am Claudette!</h2>
+}
 
-const myelement = <h1>{x < 10 ? "Hello" : "Goodbye"}</h1>
-
-ReactDOM.render(myelement, document.getElementById('root'))
+ReactDOM.render(<Car />, document.getElementById('root'))
 
 // React renders HTML to the web page by using a function called ReactDOM.render().
 // this is rendered to the div in index.html where root is found
